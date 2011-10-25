00a5c673ef6420210e82c23e5da5d829ee4ea294
Byambatsogt Tumurkhuu <pcetsogtoo@gmail.com>
2011-10-25 18:23:10 -0500

    test

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,22 @@
 tmp=/tmp/tmp$$
 
 cur_dir=$(pwd)
-git-dir=$tmp-git_dir
+git_dir=$tmp-git_dir
 site_dir=$(pwd)/site
+reponame=pcetsogtoo.github.com
+
+repo=git@github.com:pcetsogtoo/pcetsogtoo.github.com.git
 
 mkdir -p $git_dir
 cd $git_dir
 
+git clone $repo
+cd $reponame
 
+cp -rp $site_dir/* .
+git add .
+git commit -m "test"
+git push
 
+sudo rm -r $tmp-*
+exit 0
